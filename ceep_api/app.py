9f1739498451fdccd12eb2d994574f83d5baaaf9
seraph <seraph115@gmail.com>
2017-09-05 22:30:49 +0800

    fixed request to server by IP

--- ceep_api/app.py
+++ ceep_api/app.py
@@ -16,7 +16,6 @@ log = logging.getLogger(__name__)
 
 
 def configure_app(flask_app):
-    #flask_app.config['SERVER_NAME'] = settings.FLASK_HOST + ':' + str(settings.FLASK_PORT)
     flask_app.config['SQLALCHEMY_DATABASE_URI'] = settings.SQLALCHEMY_DATABASE_URI
     flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = settings.SQLALCHEMY_TRACK_MODIFICATIONS
     flask_app.config['SWAGGER_UI_DOC_EXPANSION'] = settings.RESTPLUS_SWAGGER_UI_DOC_EXPANSION
